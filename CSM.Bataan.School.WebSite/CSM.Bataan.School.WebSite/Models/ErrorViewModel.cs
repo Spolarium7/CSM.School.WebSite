2d59d9881723bc624e9e9df8acffe0aa777530a5
Goshen Jimenez <goshenjimenez@gmail.com>
2019-03-09 17:09:25 +0800

    Create Solution

---
+++ CSM.Bataan.School.WebSite/CSM.Bataan.School.WebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSM.Bataan.School.WebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
