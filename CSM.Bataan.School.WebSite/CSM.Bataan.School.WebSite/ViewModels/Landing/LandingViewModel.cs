5d4811e1f3bea44becf96add04689bab273750f1
Goshen Jimenez <goshenjimenez@gmail.com>
2019-03-17 23:29:57 +0800

    Landing Page after Login

---
+++ CSM.Bataan.School.WebSite/CSM.Bataan.School.WebSite/ViewModels/Landing/LandingViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CSM.Bataan.School.WebSite.Infrastructure.Data.BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSM.Bataan.School.WebSite.ViewModels.Landing
+{
+    public class LandingViewModel
+    {
+        public List<NewsFeedItem> Items { get; set; }
+    }
+}
