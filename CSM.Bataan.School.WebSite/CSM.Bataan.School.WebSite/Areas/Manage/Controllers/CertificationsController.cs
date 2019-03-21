0d585a560804758ea050c9f86e29b53ede0d95b3
Goshen Jimenez <goshenjimenez@gmail.com>
2019-03-22 01:05:58 +0800

    Certifications > Update Thumbnail and Banner

--- CSM.Bataan.School.WebSite/CSM.Bataan.School.WebSite/Areas/Manage/Controllers/CertificationsController.cs
+++ CSM.Bataan.School.WebSite/CSM.Bataan.School.WebSite/Areas/Manage/Controllers/CertificationsController.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using CSM.Bataan.School.WebSite.Areas.Manage.ViewModels.Certifications;
+using CSM.Bataan.School.WebSite.Areas.Manage.ViewModels.Shared;
 using CSM.Bataan.School.WebSite.Infrastructure.Data.Helpers;
 using CSM.Bataan.School.WebSite.Infrastructure.Data.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace CSM.Bataan.School.WebSite.Areas.Manage.Controllers
 {
@@ -66,6 +71,72 @@ namespace CSM.Bataan.School.WebSite.Areas.Manage.Controllers
             {
                 Certifications = result
             });
+        }
+
+        [Authorize(Policy = "AuthorizeContentAdmin")]
+        [HttpPost, Route("manage/certifications/update-{type}")]
+        public async Task<IActionResult> UpdateImage(UpdateImageViewModel model, string type)
+        {
+            var fileSize = model.ImageFile.Length;
+            if ((fileSize / 1048576.0) > 2)
+            {
+                ModelState.AddModelError("", "The file you uploaded is too large. Filesize limit is 2mb.");
+                return View(model);
+            }
+
+            if (model.ImageFile.ContentType != "image/jpeg" && model.ImageFile.ContentType != "image/png")
+            {
+                ModelState.AddModelError("", "Please upload a jpeg or png file for the thumbnail.");
+                return View(model);
+            }
+
+            var dirPath = _env.WebRootPath + "/certifications/" + model.Id.ToString();
+            if (!Directory.Exists(dirPath))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+
+            var filePath = dirPath + "/" + type + ".png";
+
+            var width = 75;
+            var height = 75;
+
+            switch (type.ToLower())
+            {
+                case "thumbnail":
+                    width = 75;
+                    height = 75;
+                    break;
+                case "banner":
+                    width = 500;
+                    height = 150;
+                    break;
+            }
+
+            if (model.ImageFile.Length > 0)
+            {
+                byte[] bytes = await FileBytes(model.ImageFile.OpenReadStream());
+                using (Image<Rgba32> image = Image.Load(bytes))
+                {
+                    image.Mutate(x => x.Resize(width, height));
+                    image.Save(filePath);
+                }
+            }
+            return RedirectPermanent("~/manage/certifications?" + model.Filters);
+        }
+
+        public async Task<byte[]> FileBytes(Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
         }
     }
 }
