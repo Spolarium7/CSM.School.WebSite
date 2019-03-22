721ba4b2bf64bdafbdb12ed64136eceb4892e2af
Goshen Jimenez <goshenjimenez@gmail.com>
2019-03-22 08:10:30 +0800

    Certifications UpdateViewModels

---
+++ CSM.Bataan.School.WebSite/CSM.Bataan.School.WebSite/Areas/Manage/ViewModels/Certifications/UpdateViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSM.Bataan.School.WebSite.Areas.Manage.ViewModels.Certifications
+{
+    public class UpdateViewModel
+    {
+        public Guid? Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime PostExpiry { get; set; }
+
+        public int Limit { get; set; }
+
+        public string Filters { get; set; }
+    }
+}
